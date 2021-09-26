e4ed256109ce4b915058605833fb7c0f12063e59
AndresRojas22 <andy_chapa01@hotmail.com>
2021-09-26 13:04:02 -0500

    Creacion tabla Refacciones

---
+++ Scripts/Tabla Refacciones.sql
@@ -0,0 +1,5 @@
+create table refacciones(
+Codigo_Barras int primary key,
+Nombre varchar(100),
+Descripcion varchar(200),
+Marca varchar(100));
--- Scripts/Tabla usuarios.sql
+++ Scripts/Tabla usuarios.sql
@@ -1,6 +1,7 @@
 create table Usuarios(
 id_Usuario int primary key,
 Nombre varchar(100),
+Contraseña varchar(100),
 ApellidoP varchar(80),
 ApellidoM varchar(80),
 F_Nacimiento varchar (15),
